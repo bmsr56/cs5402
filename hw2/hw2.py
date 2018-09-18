552e5fbb98795d2fd135e63af455ff40a39f2302
Ben Simpson <bmsr56@mst.edu>
2018-09-18 18:33:13 -0500

    whoops fuck, lost all my work

--- hw2/hw2.py
+++ hw2/hw2.py
@@ -1,5 +1,16 @@
-import pandas as pd
-import numpy as np
+import sys
+import csv
+
+def getTable(file):
+    with open(file, 'r') as csvFile:
+        csvObj = csv.reader(csvFile)
+        table = []
+        for row in csvObj:
+            table.append(row)
+    return table
 
 def main():
-    
+    print(getTable(sys.argv[1]))
+
+if __name__ == '__main__':
+    main()
