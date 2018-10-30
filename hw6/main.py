a8dd190385cf9182245775c0a40986ce301e2556
Ben Simpson <bmsr56@mst.edu>
2018-10-29 20:31:49 -0500

    HI THERE

--- hw6/main.py
+++ hw6/main.py
@@ -12,6 +12,8 @@ reader = Reader(line_format='user item rating timestamp', sep='\t')
 data = Dataset.load_from_file(file_path, reader=reader)
 
 # 5
+
+print('\n#{}-------------------------------\n'.format(5))
 data.split(n_folds=3)
 
 algo = SVD()
@@ -19,16 +21,25 @@ perf = evaluate(algo, data, measures=['RMSE', 'MAE'])
 print_perf(perf)
 
 # 6
+print('\n#{}-------------------------------\n'.format(6))
+
+
 algo = SVD(biased=False) #PMF
 perf = evaluate(algo, data, measures=['RMSE', 'MAE'])
 print_perf(perf)
 
 # 7
+print('\n#{}-------------------------------\n'.format(7))
+
+
 algo = NMF()
 perf = evaluate(algo, data, measures=['RMSE', 'MAE'])
 print_perf(perf)
 
 # 8
+print('\n#{}-------------------------------\n'.format(8))
+
+
 algo = KNNBasic(sim_options = {
     'user_based': True
     })
@@ -41,6 +52,10 @@ algo = KNNBasic(sim_options = {
     })
 perf = evaluate(algo, data, measures=['RMSE', 'MAE'])
 print_perf(perf)
+
+
+# 10 - 13
+# for f in range(1,4):
 
 # 10
 # Test each 5 algos w respect on RMSE and MAE on folds of 1
@@ -55,20 +70,28 @@ print_perf(perf)
 # Report the mean for all folds, for each of the 5 algos
 
 # 14
-algo = KNNBasic(sim_options = {
-'name':’MSD’,
-'user_based': True
-})
+print('\n#{}-------------------------------\n'.format(14))
 
 algo = KNNBasic(sim_options = {
-'name':’cosine’,
-'user_based': True
-})
+    'name':'MSD',
+    'user_based': True
+    })
 
 algo = KNNBasic(sim_options = {
-'name':’pearson’,
-'user_based': True
-})
+    'name':'cosine',
+    'user_based': True
+    })
+
+algo = KNNBasic(sim_options = {
+    'name':'pearson',
+    'user_based': True  
+    })
 
 # do and compare each of these for: 1) user  based filtering 2) item based filtering
+
 # 15
+# plot: x - number of neighbors, y - line (user, collab)
+print('\n#{}-------------------------------\n'.format(15))
+
+algo = KNNBasic(k=20, sim_options = {'name':'MSD', 'user_based': True })
+algo = KNNBasic(k=20, sim_options = {'name':'MSD', 'user_based': False })
